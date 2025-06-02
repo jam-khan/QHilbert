db28714bd2239bdb934b1a58fb9c6e8dbbba1b8b
jam-khan <jamkhan@connect.hku.hk>
2025-06-02 18:28:13 +0200

    Updated lean4 goal count

--- LeanVeri/Example1/obligation1.lean
+++ LeanVeri/Example1/obligation1.lean
@@ -1,3 +1,6 @@
+/-
+AUTO-GENERATED LEAN4 OBLIGATION BY CQOTL PROVER.
+-/
 import LeanVeri.Commons
 import LeanVeri.LinearMapPropositions
 
@@ -5,11 +8,10 @@ open LinearMap
 
 variable {𝕜 : Type*} [RCLike 𝕜]
 local notation "𝕜²" => EuclideanSpace 𝕜 (Fin 2)
-noncomputable def H : 𝕜² →ₗ[𝕜] 𝕜² := sorry
-noncomputable def ketPlus : 𝕜² := sorry
+noncomputable def H : 𝕜² →ₗ[𝕜] 𝕜² := Hadamard
+noncomputable def ketPlus : 𝕜² := ketP
 
 def ket1bra1 : 𝕜² →ₗ[𝕜] 𝕜² := ketbra1
 def ket0bra0 : 𝕜² →ₗ[𝕜] 𝕜² := ketbra0
 
-lemma obligation1: (LoewnerOrder ketbra0 (outerProduct 𝕜 (H ketPlus) (H ketPlus))) := by
-  sorry
+lemma obligation1: (LoewnerOrder ketbra0 (outerProduct 𝕜 (H ketPlus) (H ketPlus))) := sorry
