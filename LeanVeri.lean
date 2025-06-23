35186255fe3013ec98830f753de1390387a4fff6
jam-khan <jamkhan@connect.hku.hk>
2025-06-23 16:27:42 +0200

    minor fix

--- LeanVeri.lean
+++ LeanVeri.lean
@@ -2,7 +2,6 @@ import LeanVeri.Commons
 import LeanVeri.CommonsTensor
 import LeanVeri.InnerTensorProductEuclideanSpace
 import LeanVeri.LinearMapPropositions
-import LeanVeri.ManualExamples.Example4
 import LeanVeri.OuterProduct
 import LeanVeri.PartialTrace
 import LeanVeri.PiL2
